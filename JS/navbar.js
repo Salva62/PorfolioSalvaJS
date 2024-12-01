741b397dc8a40005a1fecc6e44d1515777fa5587
Salva62 <alvarez.90@hotmail.com>
2024-12-01 19:35:30 +0100

    modificacion

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,13 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const navbarLinks = document.getElementById('links-navbar');
-
-    fetch('./assets/data/datos.json')
-        .then(response => response.json())
-        .then(data => {
-            data.navItems.map((link) => {
-                navbarLinks.innerHTML += `
-                    <a href="${link.url}">${link.title}</a>
-                `;
-            });
+fetch('./assets/data/datos.json')
+    .then(response => response.json())
+    .then(data => {
+        data.navItems.map((link) => {
+            navbarLinks.innerHTML += `<a href="${link.url}">${link.title}</a>`;
         });
-});
+    });
--- JS/proyectos.js
+++ JS/proyectos.js
@@ -1,19 +1,14 @@
-const proyectoContainer = document.getElementById ('grid-2')
-
-fetch("/assets/data/datos.json")
-.then(response => response.json())
-.then(data => {
-    data.proyectos.map((item) => {
-        proyectoContainer.innerHTML +=
-        `
-        <div class="proyectos-container" style="background-image: url(${item.imagen});">
-            <div class="descripción">
-                <h3>${item.nombre}</h3>
-                <P>${item.descripcion}</P>
-                <small>${item.tecnologías}</small>
-            </div>
-        </div>    
-        `
-    })
-})
-
+fetch('./assets/data/datos.json')
+    .then(response => response.json())
+    .then(data => {
+        data.proyectos.map((item) => {
+            proyectoContainer.innerHTML += `
+            <div class="proyectos-container" style="background-image: url(${item.imagen});">
+                <div class="descripción">
+                    <h3>${item.nombre}</h3>
+                    <p>${item.descripcion}</p>
+                    <small>${item.tecnologías}</small>
+                </div>
+            </div>`;
+        });
+    });
